422681a53d2bb1bcf451185b1a2270f34782562c
ARkhan214 <abdurrahimkhan214dc@gmail.com>
2026-02-18 23:42:13 +0600

    first push
    Ramadan Website

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple Auto Play Speed Control
+var myCarousel = document.querySelector('#ramadanSlider')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 2000,
+  wrap: true
+})
